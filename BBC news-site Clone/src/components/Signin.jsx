0e1baff1478c7adda9c38519c51cba2cf5c39829
saket <saket@saketgupta.com>
2024-01-24 21:15:40 +0530

    firebase-Authentication integrated

--- BBC news-site Clone/src/components/Signin.jsx
+++ BBC news-site Clone/src/components/Signin.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import frontimg from "../images/frontimg.png";
-import logo from "../images/logo_2.png"
+import logo from "../images/logo_2.png";
+import {signInWithPopup} from "firebase/auth";
+import { auth,googleProvider } from '../firebase/setup';
 
 const Signin = () => {
+  const googleSignin=async()=>{
+    try {
+      await signInWithPopup(auth,googleProvider);
+      
+    } catch (error) {
+      console.log(error);
+    }
+    // console.log(auth);
+  }
   return (
     <div className='bg-black grid grid-cols-2 h-screen'>
       {/* creating the left part */}
       <div className='bg-black text-center'>
-        <img className='h-16 ml-[18rem] mt-40 rounded-none' src={logo} alt="" />
-        <h1 className='text-white text-3xl mt-7 font-bold'>Sign in</h1>
-        <button type="button" class="text-white mt-8 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium text-xl px-5 py-2.5 text-center w-80 hover:scale-110 ">Next</button>
+        <img className='h-16 ml-[18rem] mt-40 rounded-none cursor-pointer' src={logo} alt="" />
+        <h1  className='text-white text-3xl mt-7 font-bold'>Sign in</h1>
+        <button onClick={googleSignin} type="button" class="text-white mt-8 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium text-xl px-5 py-2.5 text-center w-80 hover:scale-110 ">Next</button>
         <h1 className='text-blue-400 underline hover:text-blue-700 mt-6 text-lg font-semibold'>Sign In now</h1>
       </div>
 
@@ -20,4 +31,4 @@ const Signin = () => {
   )
 }
 
-export default Signin
+export default Signin;
---
+++ BBC news-site Clone/src/firebase/setup.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth,GoogleAuthProvider} from "firebase/auth"
+const firebaseConfig = {
+  apiKey: "AIzaSyAsLyXsNLyi0U-SoUlFhUzgQJaYenbuVGs",
+  authDomain: "bbc-clone-1227b.firebaseapp.com",
+  projectId: "bbc-clone-1227b",
+  storageBucket: "bbc-clone-1227b.appspot.com",
+  messagingSenderId: "73930595236",
+  appId: "1:73930595236:web:cca7fe74f30385c43dff31"
+};
+const app = initializeApp(firebaseConfig);
+export const auth=getAuth(app);
+export const googleProvider=new GoogleAuthProvider(app);
